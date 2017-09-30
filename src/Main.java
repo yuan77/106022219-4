090803d8c0a0465f055646ebad45aca78d13ed3b
yuan77 <sky58029@gmail.com>
2017-09-30 14:40:14 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("請輸入欲轉換之英哩數:");
+        Scanner sc = new Scanner(System.in);
+        int keyin = sc.nextInt();
+        double sum = 0;
+        sum = keyin*1.6;
+
+        System.out.println("公里數:"+sum);
+        // write your code here
+    }
+}
